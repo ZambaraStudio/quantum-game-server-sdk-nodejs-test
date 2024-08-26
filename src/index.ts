624f1334f3af6ea79cb786fd6f71818001f75c5f
levansuper <l.basharuli@gmail.com>
2024-08-26 13:24:37 +0400

    new updaate

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-import { QuantumGameServer, buildSocketServer } from 'quantum-game-server-sdk';
+import { buildQuantumGameServer } from "quantum-game-server-sdk";
 
+const quantumGameServer = buildQuantumGameServer();
 
+quantumGameServer.on("connect", () => {
+  console.log("CONNECTED!!!!", quantumGameServer.qsGSServerId);
+});
 
-const quantumGameServer = buildSocketServer();
+quantumGameServer.on("player-connected", () => {
+  console.log("sadasdsa");
+});
+quantumGameServer.on('generic-message', (message) => {
+  console.log(message, 11112222, "This is iT!");
+  // quantumGameServer.sendGenericMessage({
+  //   data: 'asdasdasd'
+  // });
+})
 
+quantumGameServer.on("connect", () => {
+  quantumGameServer.sendGenericMessage({
+    data: "asdasdasd",
+  });
+});
 
 quantumGameServer.start();
-
-
-quantumGameServer.observable.subscribe((a) => {
-  console.log(1111, a)
-})
